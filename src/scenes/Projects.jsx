07f5d7d41fe3bd95dc7c5df4b480f7cd870cc5c7
Fendatam <89171845+Fendatam@users.noreply.github.com>
2023-01-10 07:05:25 +0300

    Add files via upload

--- src/scenes/Projects.jsx
+++ src/scenes/Projects.jsx
@@ -25,57 +25,52 @@ const Project = ({ title, subtitle }) => {
       <div className={overlayStyles}>
         <p className="text-2xl font-playfair">{title}</p>
         <p className="mt-7">
-            {subtitle}
+          {subtitle}
         </p>
       </div>
-      <img src={`../assets/${projectTitle}.jpg`} alt={projectTitle} className=" lg:max-w-[600px] max-w-[350px] lg:max-h-[600px] max-h-[300px]"/>
+      <img src={`../assets/${projectTitle}.jpg`} alt={projectTitle} className=" lg:max-w-[600px] max-w-[350px] lg:max-h-[600px] max-h-[300px]" />
     </motion.div>
   );
 };
 
 const Projects = () => {
-    return (
-        <section id="projects" className="lg:py-48 py-24 slo4">
-            {/* hing */}
-                <motion.div
-                    className="w-2/5 mx-auto text-center"
-                    initial="hidden"
-                    whileInView="visible"
-                    viewport={{ once: true, amount: 0.5 }}
-                    transition={{ duration: 0.5 }}
-                    variants={{
-                    hidden: { opacity: 0, y: -50 },
-                    visible: { opacity: 1, y: 0 }
-                    }}>
-                    <p className=" font-playfair font-semibold lg:text-5xl text-3xl">
-                      <span className="text-blue">PRO</span>JECTS
-                </p>
-                <div className=" flex justify-center lg:mt-5 mt-2">
-                    <LineGradient width="w-1/3" />
-                </div>
-            </motion.div>
-            <div className="flex justify-center lg:mt-16 mt-8">
-             <motion.div
-                    className="sm:grid sm:grid-cols-2"
-                    initial="hidden"
-                    whileInView="visible"
-                    viewport={{ once: true, amount: 0.2 }}
-                    transition={{ duration: 0.5 }}
-                   variants={container}>
-                <div
-            className="flex justify-center text-center items-center lg:p-10 p-5 bg-blue
-            lg:max-w-[600px] max-w-[350px] lg:max-h-[600px] max-h-[300px] text-2xl font-playfair font-semibold"
-          >
-            BEAUTIFUL USER INTERFACES
-          </div>
-                    <a href="https://arab-builders.vercel.app" target="_blank" rel="noreferrer"><Project title="Project 1" subtitle="A website I designed for a construction company. I used React.js, Framer motion, react slick slider, tailwindcss and email.js"/></a>
-                     {/* ROW 2 */}
-                    <Project title="Project 2" subtitle="Simple Responsive Website using HTML, CSS and BootStrap"/>
-                    <a href="https://netflix-clone907.vercel.app/" target="_blank" rel="noreferrer"><Project title="Project 3" subtitle="Netflix Landing page clone + Sign-in page using React.js and tailwind"/></a>
-                    </motion.div>
-                    </div>
-        </section>
-    )
+  return (
+    <section id="projects" className="lg:py-48 py-24 slo4">
+      {/* hing */}
+      <motion.div
+        className="w-2/5 mx-auto text-center"
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.5 }}
+        transition={{ duration: 0.5 }}
+        variants={{
+          hidden: { opacity: 0, y: -50 },
+          visible: { opacity: 1, y: 0 }
+        }}>
+        <p className=" font-playfair font-semibold lg:text-5xl text-3xl">
+          <span className="text-blue">PRO</span>JECTS
+        </p>
+        <div className=" flex justify-center lg:mt-5 mt-2">
+          <LineGradient width="w-1/3" />
+        </div>
+      </motion.div>
+      <div className="flex justify-center lg:mt-16 mt-8">
+        <motion.div
+          className="sm:grid sm:grid-cols-2"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.2 }}
+          transition={{ duration: 0.5 }}
+          variants={container}>
+          <a href="https://mj-sneaker-store.vercel.app" target="_blank" rel="noreferrer"><Project title="Project 4" subtitle="An ecommerce website I designed for a shoe company. I used Next.js for handling the frontend and backend, tailwind for styling and responsiveness, sanity for content management allowing the owner to update their products without coding. ALso used stripe to handle payments and react hot toast and react icons." /></a>
+          <a href="https://arab-builders.vercel.app" target="_blank" rel="noreferrer"><Project title="Project 1" subtitle="A website I designed for a construction company. I used React.js, Framer motion, react slick slider, tailwindcss and email.js" /></a>
+          {/* ROW 2 */}
+          <Project title="Project 2" subtitle="Simple Responsive Website using HTML, CSS and BootStrap" />
+          <a href="https://netflix-clone907.vercel.app/" target="_blank" rel="noreferrer"><Project title="Project 3" subtitle="Netflix Landing page clone + Sign-in page using React.js and tailwind" /></a>
+        </motion.div>
+      </div>
+    </section>
+  )
 };
 
 export default Projects;
